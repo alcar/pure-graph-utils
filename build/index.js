9d2bab6541a54f82ee742e333860a2153a633cdd
Alan Carpilovsky <8486092+alcar@users.noreply.github.com>
2019-07-20 12:55:50 -0300

    Update build

--- build/index.js
+++ build/index.js
@@ -1,12 +1,21 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.removeNode = exports.removeAllConnections = exports.removeConnection = exports.addConnection = exports.removeGraphNodeProperty = exports.editGraphNodeProperty = exports.addGraphNodeProperty = exports.changeGraphNodeId = exports.addNode = exports.createGraph = exports.removeNodeProperty = exports.editNodeProperty = exports.addNodeProperty = exports.changeNodeId = exports.createNode = void 0;
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -16,19 +25,24 @@ var removeObjectProperty = function removeObjectProperty(propKey, targetObject) 
   return Object.keys(targetObject).filter(function (key) {
     return key !== propKey;
   }).reduce(function (objectAccumulator, currentPropKey) {
-    return _extends({}, objectAccumulator, _defineProperty({}, currentPropKey, targetObject[currentPropKey]));
+    return _objectSpread({}, objectAccumulator, _defineProperty({}, currentPropKey, targetObject[currentPropKey]));
   }, {});
 };
 
-var createNode = exports.createNode = function createNode(id, value) {
+var createNode = function createNode(id, value) {
   if (typeof id === 'undefined') {
     return EMPTY_OBJECT;
   }
 
-  return { id: id, value: value };
+  return {
+    id: id,
+    value: value
+  };
 };
 
-var changeNodeId = exports.changeNodeId = function changeNodeId(newId, node) {
+exports.createNode = createNode;
+
+var changeNodeId = function changeNodeId(newId, node) {
   if (typeof node === 'undefined') {
     return createNode();
   }
@@ -37,10 +51,14 @@ var changeNodeId = exports.changeNodeId = function changeNodeId(newId, node) {
     return node;
   }
 
-  return _extends({}, node, { id: newId });
+  return _objectSpread({}, node, {
+    id: newId
+  });
 };
 
-var addNodeProperty = exports.addNodeProperty = function addNodeProperty(propKey, propValue, node) {
+exports.changeNodeId = changeNodeId;
+
+var addNodeProperty = function addNodeProperty(propKey, propValue, node) {
   if (typeof node === 'undefined') {
     return createNode();
   }
@@ -49,10 +67,12 @@ var addNodeProperty = exports.addNodeProperty = function addNodeProperty(propKey
     return node;
   }
 
-  return _extends({}, node, _defineProperty({}, propKey, propValue));
+  return _objectSpread({}, node, _defineProperty({}, propKey, propValue));
 };
 
-var editNodeProperty = exports.editNodeProperty = function editNodeProperty(propKey, propNewValue, node) {
+exports.addNodeProperty = addNodeProperty;
+
+var editNodeProperty = function editNodeProperty(propKey, propNewValue, node) {
   if (typeof node === 'undefined') {
     return createNode();
   }
@@ -61,10 +81,12 @@ var editNodeProperty = exports.editNodeProperty = function editNodeProperty(prop
     return node;
   }
 
-  return _extends({}, node, _defineProperty({}, propKey, propNewValue));
+  return _objectSpread({}, node, _defineProperty({}, propKey, propNewValue));
 };
 
-var removeNodeProperty = exports.removeNodeProperty = function removeNodeProperty(propKey, node) {
+exports.editNodeProperty = editNodeProperty;
+
+var removeNodeProperty = function removeNodeProperty(propKey, node) {
   if (typeof node === 'undefined') {
     return createNode();
   }
@@ -76,17 +98,22 @@ var removeNodeProperty = exports.removeNodeProperty = function removeNodePropert
   return removeObjectProperty(propKey, node);
 };
 
-var createGraph = exports.createGraph = function createGraph(node) {
+exports.removeNodeProperty = removeNodeProperty;
+
+var createGraph = function createGraph(node) {
   if (typeof node === 'undefined' || typeof node.id === 'undefined') {
     return EMPTY_OBJECT;
   }
 
-  var completeNode = !node.connections || node.connections.length > 0 ? _extends({}, node, { connections: [] }) : node;
-
+  var completeNode = !node.connections || node.connections.length > 0 ? _objectSpread({}, node, {
+    connections: []
+  }) : node;
   return _defineProperty({}, node.id, completeNode);
 };
 
-var addNode = exports.addNode = function addNode(node, graph) {
+exports.createGraph = createGraph;
+
+var addNode = function addNode(node, graph) {
   if (typeof graph === 'undefined') {
     return createGraph();
   }
@@ -95,12 +122,15 @@ var addNode = exports.addNode = function addNode(node, graph) {
     return graph;
   }
 
-  var completeNode = !node.connections || node.connections.length > 0 ? _extends({}, node, { connections: [] }) : node;
-
-  return _extends({}, graph, _defineProperty({}, node.id, completeNode));
+  var completeNode = !node.connections || node.connections.length > 0 ? _objectSpread({}, node, {
+    connections: []
+  }) : node;
+  return _objectSpread({}, graph, _defineProperty({}, node.id, completeNode));
 };
 
-var changeGraphNodeId = exports.changeGraphNodeId = function changeGraphNodeId(nodeId, nodeNewId, graph) {
+exports.addNode = addNode;
+
+var changeGraphNodeId = function changeGraphNodeId(nodeId, nodeNewId, graph) {
   if (typeof graph === 'undefined') {
     return createGraph();
   }
@@ -110,19 +140,21 @@ var changeGraphNodeId = exports.changeGraphNodeId = function changeGraphNodeId(n
   }
 
   var graphWithUpdatedConnections = graph[nodeId].connections.reduce(function (graphAccumulator, currentId) {
-    return _extends({}, graphAccumulator, _defineProperty({}, currentId, _extends({}, graph[currentId], {
+    return _objectSpread({}, graphAccumulator, _defineProperty({}, currentId, _objectSpread({}, graph[currentId], {
       connections: graph[currentId].connections.map(function (id) {
         return id === nodeId ? nodeNewId : id;
       })
     })));
   }, graph);
-
   var graphWithoutNodeId = removeObjectProperty(nodeId, graphWithUpdatedConnections);
-
-  return _extends({}, graphWithoutNodeId, _defineProperty({}, nodeNewId, _extends({}, graph[nodeId], { id: nodeNewId })));
+  return _objectSpread({}, graphWithoutNodeId, _defineProperty({}, nodeNewId, _objectSpread({}, graph[nodeId], {
+    id: nodeNewId
+  })));
 };
 
-var addGraphNodeProperty = exports.addGraphNodeProperty = function addGraphNodeProperty(nodeId, propKey, propValue, graph) {
+exports.changeGraphNodeId = changeGraphNodeId;
+
+var addGraphNodeProperty = function addGraphNodeProperty(nodeId, propKey, propValue, graph) {
   if (typeof graph === 'undefined') {
     return createGraph();
   }
@@ -131,10 +163,12 @@ var addGraphNodeProperty = exports.addGraphNodeProperty = function addGraphNodeP
     return graph;
   }
 
-  return _extends({}, graph, _defineProperty({}, nodeId, _extends({}, graph[nodeId], _defineProperty({}, propKey, propValue))));
+  return _objectSpread({}, graph, _defineProperty({}, nodeId, _objectSpread({}, graph[nodeId], _defineProperty({}, propKey, propValue))));
 };
 
-var editGraphNodeProperty = exports.editGraphNodeProperty = function editGraphNodeProperty(nodeId, propKey, propNewValue, graph) {
+exports.addGraphNodeProperty = addGraphNodeProperty;
+
+var editGraphNodeProperty = function editGraphNodeProperty(nodeId, propKey, propNewValue, graph) {
   if (typeof graph === 'undefined') {
     return createGraph();
   }
@@ -143,10 +177,12 @@ var editGraphNodeProperty = exports.editGraphNodeProperty = function editGraphNo
     return graph;
   }
 
-  return _extends({}, graph, _defineProperty({}, nodeId, _extends({}, graph[nodeId], _defineProperty({}, propKey, propNewValue))));
+  return _objectSpread({}, graph, _defineProperty({}, nodeId, _objectSpread({}, graph[nodeId], _defineProperty({}, propKey, propNewValue))));
 };
 
-var removeGraphNodeProperty = exports.removeGraphNodeProperty = function removeGraphNodeProperty(nodeId, propKey, graph) {
+exports.editGraphNodeProperty = editGraphNodeProperty;
+
+var removeGraphNodeProperty = function removeGraphNodeProperty(nodeId, propKey, graph) {
   if (typeof graph === 'undefined') {
     return createGraph();
   }
@@ -155,11 +191,13 @@ var removeGraphNodeProperty = exports.removeGraphNodeProperty = function removeG
     return graph;
   }
 
-  return _extends({}, graph, _defineProperty({}, nodeId, removeObjectProperty(propKey, graph[nodeId])));
+  return _objectSpread({}, graph, _defineProperty({}, nodeId, removeObjectProperty(propKey, graph[nodeId])));
 };
 
-var addConnection = exports.addConnection = function addConnection(nodeAId, nodeBId, graph) {
-  var _extends13;
+exports.removeGraphNodeProperty = removeGraphNodeProperty;
+
+var addConnection = function addConnection(nodeAId, nodeBId, graph) {
+  var _objectSpread13;
 
   if (typeof graph === 'undefined') {
     return createGraph();
@@ -169,15 +207,17 @@ var addConnection = exports.addConnection = function addConnection(nodeAId, node
     return graph;
   }
 
-  return _extends({}, graph, (_extends13 = {}, _defineProperty(_extends13, nodeAId, _extends({}, graph[nodeAId], {
+  return _objectSpread({}, graph, (_objectSpread13 = {}, _defineProperty(_objectSpread13, nodeAId, _objectSpread({}, graph[nodeAId], {
     connections: [].concat(_toConsumableArray(graph[nodeAId].connections), [nodeBId])
-  })), _defineProperty(_extends13, nodeBId, _extends({}, graph[nodeBId], {
+  })), _defineProperty(_objectSpread13, nodeBId, _objectSpread({}, graph[nodeBId], {
     connections: [].concat(_toConsumableArray(graph[nodeBId].connections), [nodeAId])
-  })), _extends13));
+  })), _objectSpread13));
 };
 
-var removeConnection = exports.removeConnection = function removeConnection(nodeAId, nodeBId, graph) {
-  var _extends14;
+exports.addConnection = addConnection;
+
+var removeConnection = function removeConnection(nodeAId, nodeBId, graph) {
+  var _objectSpread14;
 
   if (typeof graph === 'undefined') {
     return createGraph();
@@ -187,18 +227,20 @@ var removeConnection = exports.removeConnection = function removeConnection(node
     return graph;
   }
 
-  return _extends({}, graph, (_extends14 = {}, _defineProperty(_extends14, nodeAId, _extends({}, graph[nodeAId], {
+  return _objectSpread({}, graph, (_objectSpread14 = {}, _defineProperty(_objectSpread14, nodeAId, _objectSpread({}, graph[nodeAId], {
     connections: graph[nodeAId].connections.filter(function (id) {
       return id !== nodeBId;
     })
-  })), _defineProperty(_extends14, nodeBId, _extends({}, graph[nodeBId], {
+  })), _defineProperty(_objectSpread14, nodeBId, _objectSpread({}, graph[nodeBId], {
     connections: graph[nodeBId].connections.filter(function (id) {
       return id !== nodeAId;
     })
-  })), _extends14));
+  })), _objectSpread14));
 };
 
-var removeAllConnections = exports.removeAllConnections = function removeAllConnections(nodeId, graph) {
+exports.removeConnection = removeConnection;
+
+var removeAllConnections = function removeAllConnections(nodeId, graph) {
   if (typeof graph === 'undefined') {
     return createGraph();
   }
@@ -212,7 +254,9 @@ var removeAllConnections = exports.removeAllConnections = function removeAllConn
   }, graph);
 };
 
-var removeNode = exports.removeNode = function removeNode(nodeId, graph) {
+exports.removeAllConnections = removeAllConnections;
+
+var removeNode = function removeNode(nodeId, graph) {
   if (typeof graph === 'undefined') {
     return createGraph();
   }
@@ -222,6 +266,7 @@ var removeNode = exports.removeNode = function removeNode(nodeId, graph) {
   }
 
   var graphWithoutNodeIdConnections = removeAllConnections(nodeId, graph);
-
   return removeObjectProperty(nodeId, graphWithoutNodeIdConnections);
 };
+
+exports.removeNode = removeNode;
